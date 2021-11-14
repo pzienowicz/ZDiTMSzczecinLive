1778d77036d1a5335556cb607c31a1222a51b64d
Paweł Zienowicz <zienowicz.pawel@gmail.com>
2021-11-14 20:57:51 +0100

    update ACRA configuration

--- app/src/main/java/pl/pzienowicz/zditmszczecinlive/MyApplication.kt
+++ app/src/main/java/pl/pzienowicz/zditmszczecinlive/MyApplication.kt
@@ -3,23 +3,23 @@ package pl.pzienowicz.zditmszczecinlive
 import android.app.Application
 import android.content.Context
 
-import org.acra.ACRA
-import org.acra.annotation.AcraCore
-import org.acra.annotation.AcraHttpSender
+import org.acra.config.httpSender
+import org.acra.ktx.initAcra
 
-@AcraCore(buildConfigClass = BuildConfig::class)
-@AcraHttpSender(
-        httpMethod = org.acra.sender.HttpSender.Method.PUT,
-        uri = Config.ACRA_URL,
-        basicAuthLogin = Config.ACRA_USER,
-        basicAuthPassword = Config.ACRA_PASS
-)
 class MyApplication : Application() {
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
 
-        // The following line triggers the initialization of ACRA
-        ACRA.init(this)
+        initAcra {
+            //core configuration:
+            buildConfigClass = BuildConfig::class.java
+            httpSender {
+                httpMethod = org.acra.sender.HttpSender.Method.PUT
+                uri = Config.ACRA_URL
+                basicAuthLogin = Config.ACRA_USER
+                basicAuthPassword = Config.ACRA_PASS
+            }
+        }
     }
 }
