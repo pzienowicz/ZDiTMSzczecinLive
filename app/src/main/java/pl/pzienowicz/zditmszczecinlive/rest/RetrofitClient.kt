36eda862a0badd0021efd192db3566200748d324
Paweł Zienowicz <zienowicz.pawel@gmail.com>
2022-09-19 18:03:11 +0200

    added logging interceptor

--- app/src/main/java/pl/pzienowicz/zditmszczecinlive/rest/RetrofitClient.kt
+++ app/src/main/java/pl/pzienowicz/zditmszczecinlive/rest/RetrofitClient.kt
@@ -1,53 +1,24 @@
 package pl.pzienowicz.zditmszczecinlive.rest
 
-import pl.pzienowicz.zditmszczecinlive.Config
-import retrofit2.converter.gson.GsonConverterFactory
 import okhttp3.OkHttpClient
-import java.security.cert.CertificateException
-import javax.net.ssl.*
+import okhttp3.logging.HttpLoggingInterceptor
+import pl.pzienowicz.zditmszczecinlive.Config
 import retrofit2.Retrofit
-import java.security.cert.X509Certificate
-
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
     fun getRetrofit(): Retrofit {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor) // <-- this is the importan
+
         return Retrofit.Builder()
                 .baseUrl(Config.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
-//                .client(getUnsafeOkHttpClient())
+//                .client(httpClient.build())
                 .build()
-    }
-
-    private fun getUnsafeOkHttpClient(): OkHttpClient {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-                override fun getAcceptedIssuers(): Array<X509Certificate> {
-                    return arrayOf()
-                }
-
-                @Throws(CertificateException::class)
-                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
-
-                @Throws(CertificateException::class)
-                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
-            })
-
-            // Install the all-trusting trust manager
-            val sslContext = SSLContext.getInstance("SSL")
-            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
-            // Create an ssl socket factory with our all-trusting manager
-            val sslSocketFactory = sslContext.socketFactory
-
-            val builder = OkHttpClient.Builder()
-            builder.sslSocketFactory(sslSocketFactory)
-            builder.hostnameVerifier { hostname, session -> true }
-
-            return builder.build()
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
-
     }
 }
