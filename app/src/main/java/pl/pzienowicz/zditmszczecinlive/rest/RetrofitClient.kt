95725b8a4c18f123353abcaac33865b708740752
Pawcik <pavlo.asto@gmail.com>
2026-07-19 23:14:22 +0200

    Add ZDiTM API user agent

--- app/src/main/java/pl/pzienowicz/zditmszczecinlive/rest/RetrofitClient.kt
+++ app/src/main/java/pl/pzienowicz/zditmszczecinlive/rest/RetrofitClient.kt
@@ -1,7 +1,6 @@
 package pl.pzienowicz.zditmszczecinlive.rest
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import pl.pzienowicz.zditmszczecinlive.Config
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,19 +8,25 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    fun getRetrofit(): Retrofit {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
+    private const val USER_AGENT = "Komunikacja Miejska Szczecin (aplikacja nieoficjalna)"
 
+    fun getRetrofit(): Retrofit {
         val httpClient = OkHttpClient.Builder()
-        httpClient.addInterceptor(interceptor)
+        httpClient.addInterceptor { chain ->
+            val request = chain.request()
+                .newBuilder()
+                .header("User-Agent", USER_AGENT)
+                .build()
+
+            chain.proceed(request)
+        }
         httpClient.readTimeout(60, TimeUnit.SECONDS)
         httpClient.connectTimeout(60, TimeUnit.SECONDS)
 
         return Retrofit.Builder()
                 .baseUrl(Config.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
-//                .client(httpClient.build())
+                .client(httpClient.build())
                 .build()
     }
 }
